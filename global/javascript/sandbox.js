5512461f76f887e8ccb6dfa39b2939ea155f48a0
Quynh <nguyenquynhanh17890@gmail.com>
2023-01-07 18:39:03 +0100

    still dont know how commit messages work

--- global/javascript/sandbox.js
+++ global/javascript/sandbox.js
@@ -1,1 +1,14 @@
-// for playing around
+function openPage(pageName, elmnt, color) {
+      // Hide all elements with class="tabcontent" by default */
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+    
+      // Show the specific tab content
+      document.getElementById(pageName).style.display = "flex";
+    }
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();     
